1066f61a88a1e7297e9e262e553341e2c2ae347f
JulianaChM <julianchamorromarin@gmail.com>
2024-04-23 19:00:15 -0500

    feat: port configuration

--- backend/index.js
+++ backend/index.js
@@ -3,3 +3,11 @@ const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
