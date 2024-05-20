c7196cec60e7799e6d945fa1cf59066bd11f2e23
JulianaChM <julianchamorromarin@gmail.com>
2024-05-20 09:26:48 -0500

    feat: drivers tasks drag and drop

---
+++ frontend/src/components/drivers/TaskComponent.js
@@ -0,0 +1,95 @@
+import { useState } from "react";
+import React from "react";
+import ReactDOM from "react-dom";
+import './TaskComponent.css'
+export const DragAndDrop = () => {
+  const [tasks, setTasks] = useState([
+    { id: 1, title: "Tarea 1", body: "Cuerpo de la tarea 1", list:1 }, 
+    { id: 2, title: "Tarea 2", body: "Cuerpo de la tarea 2", list:1 },
+    { id: 3, title: "Tarea 3", body: "Cuerpo de la tarea 3", list:1 },
+    { id: 4, title: "Tarea 4", body: "Cuerpo de la tarea 4", list:1 }, 
+  ]);
+
+  const getList = (list) => {
+    return tasks.filter(item => item.list === list)
+}
+
+const startDrag = (evt, item) => {
+    evt.dataTransfer.setData('itemID', item.id)
+    console.log(item);
+}
+
+const draggingOver = (evt) => {
+    evt.preventDefault();
+}
+
+const onDrop = (evt, list) => {
+    const itemID = evt.dataTransfer.getData('itemID');
+    const item = tasks.find(item => item.id == itemID);
+    item.list = list;
+
+    const newState = tasks.map(task => {
+        if(task.id === itemID) return item;
+        return task
+    })
+
+    setTasks(newState);
+}
+
+return (
+    <>
+        <h1>
+            Arrastrar y Soltar &nbsp;
+        </h1>
+        <br/>
+
+        <div className='drag-and-drop'>
+            <div className='column column--1'>
+                <h3>
+                    Tareas por hacer
+                </h3>
+                <div className='dd-zone' droppable="true" onDragOver={(evt => draggingOver(evt))} onDrop={(evt => onDrop(evt, 1))}>
+                    {getList(1).map(item => (
+                        <div className='dd-element' key={item.id} draggable onDragStart={(evt) => startDrag(evt, item)}>
+                            <strong className='title'>{item.title}</strong>
+                            <p className='body'>{item.body}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            <div className='column column--2'>
+                <h3>
+                    Tareas en progreso
+                </h3>
+                <div className='dd-zone' droppable="true" onDragOver={(evt => draggingOver(evt))} onDrop={(evt => onDrop(evt, 2))}>
+                    {getList(2).map(item => (
+                        <div className='dd-element' key={item.id} draggable onDragStart={(evt) => startDrag(evt, item)}>
+                            <strong className='title'>{item.title}</strong>
+                            <p className='body'>{item.body}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            <div className='column column--3'>
+                <h3>
+                    Tareas realizadas
+                </h3>
+                <div className='dd-zone' droppable="true" onDragOver={(evt => draggingOver(evt))} onDrop={(evt => onDrop(evt, 3))}>
+                    {getList(3).map(item => (
+                        <div className='dd-element' key={item.id} draggable onDragStart={(evt) => startDrag(evt, item)}>
+                            <strong className='title'>{item.title}</strong>
+                            <p className='body'>{item.body}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    </>
+)
+}
+
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<DragAndDrop />, rootElement);
