5c0d8c3c4c1973c5ed99c8d35d7d86ecc0ae008d
Jhon <jhon.rodriguez22329@ucaldas.edu.co>
2024-05-25 17:21:42 -0500

    feat: side menu working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
-import { CreateUserComponent } from './components/users/CreateUserComponent';
-import { ListComponent } from './components/users/ListComponent';
-import { DragAndDrop } from './components/drivers/TaskComponent.js';
+import { AdminLayout } from './layouts/adminLayouts/AdminLayout.js';
+import { AdminRoutes } from './routes/AppRoute.js';
+import { BrowserRouter } from "react-router-dom";
+// import { CreateUserComponent } from './components/users/CreateUserComponent';
+// import { ListComponent } from './components/users/ListComponent';
+// import { DragAndDrop } from './pages/adminPages/drivers/TaskComponent.js';
 
 function App() {
   return (
+  <BrowserRouter>
+    <AdminRoutes/>
+  </BrowserRouter>
+  
+  
   // <CreateUserComponent />
   /* <ListComponent/> */
-  <DragAndDrop/>
-);
+  // <DragAndDrop/>
+  // <AdminLayout/>
+  );
 }
 
 export default App;
