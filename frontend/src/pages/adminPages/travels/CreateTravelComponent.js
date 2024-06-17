b60ba94b292782fae9ca83f32c94b225a6a0e487
JulianaChM <julianchamorromarin@gmail.com>
2024-06-17 01:01:06 -0500

    feat: draw route

--- frontend/src/pages/adminPages/travels/CreateTravelComponent.js
+++ frontend/src/pages/adminPages/travels/CreateTravelComponent.js
@@ -1,14 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
-import { Button, Input, Select, Form } from "antd";
+import { Button, Input, Select, Form, Modal } from "antd";
 import { PATHS } from "../../../utils/config";
 import axios from "axios";
 import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import AsyncSelect from 'react-select/async';
 import L from "leaflet";
+import "leaflet-routing-machine";
 import iconGreen from "../../../uploads/marks/green-mark.svg";
 import iconRed from "../../../uploads/marks/red-mark.svg";
+import AsyncSelect from 'react-select/async';
+import './CreateTravelComponent.css';
 
 const { Option } = Select;
 
@@ -19,6 +21,9 @@ export const CreateTravelComponent = () => {
   const [destination, setDestination] = useState(null);
   const [mapCenter, setMapCenter] = useState([4.570868, -74.297333]); // Center map on Colombia
   const [mapReady, setMapReady] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [routingControl, setRoutingControl] = useState(null);
+  const [instructions, setInstructions] = useState('');
 
   const [trucks, setTrucks] = useState([]);
   const [selectedTruck, setSelectedTruck] = useState('');
@@ -26,7 +31,6 @@ export const CreateTravelComponent = () => {
   const [drivers, setDrivers] = useState([]);
   const [selectedDriver, setSelectedDriver] = useState('');
 
-  // Define custom icons
   const originIcon = L.icon({
     iconUrl: iconGreen, 
     iconSize: [25, 41], 
@@ -49,7 +53,6 @@ export const CreateTravelComponent = () => {
     }
     try {
       const response = await axios.get(`https://graphhopper.com/api/1/geocode?q=${inputValue}, Colombia&locale=es&key=bc2c1444-a0c2-4cad-8814-6f196228c9fe`);
-      console.log("API Response:", response.data);
       return response.data.hits.map(hit => ({
         label: `${hit.name}, ${hit.country}`,
         value: [hit.point.lat, hit.point.lng],
@@ -93,19 +96,14 @@ export const CreateTravelComponent = () => {
 
     try {
       const URL = PATHS.BASE_PATH + PATHS.API_ROUTES.CREATE_TRAVEL;
-      console.log("Entra la url", URL);
-      console.log("Travel data:", travel);
-
       const response = await axios.post(URL, travel, {
         headers: {
             'Content-Type': 'application/json'
         }
       });
       console.log(response);
-      
     } catch (error) {
       console.error("Error creating travel", error);
-      console.error("Response data:", error.response?.data);
     }
   };
 
@@ -117,7 +115,6 @@ export const CreateTravelComponent = () => {
   const fetchDrivers = async () =>  {
     try {
       const URL = PATHS.BASE_PATH + PATHS.API_ROUTES.LIST_USERS;
-      console.log(URL);
       const response = await axios.get(URL);
       const drivers = response.data.filter((driver) => driver.rol === 'Conductor');
       setDrivers(drivers);
@@ -146,8 +143,69 @@ export const CreateTravelComponent = () => {
     return null;
   };
 
+  const RouteDrawer = ({ origin, destination }) => {
+    const map = useMap();
+    const instructionsRef = useRef();
+
+    useEffect(() => {
+      if (origin && destination) {
+        if (routingControl) {
+          routingControl.getPlan().setWaypoints([
+            L.latLng(origin[0], origin[1]),
+            L.latLng(destination[0], destination[1])
+          ]);
+        } else {
+          const control = L.Routing.control({
+            waypoints: [
+              L.latLng(origin[0], origin[1]),
+              L.latLng(destination[0], destination[1])
+            ],
+            language: 'es',
+            createMarker: function(i, wp) {
+              return L.marker(wp.latLng, {
+                icon: i === 0 ? originIcon : destinationIcon
+              });
+            },
+            routeWhileDragging: true,
+            lineOptions: {
+              styles: [{ color: '#5980ad', opacity: 1, weight: 5 }]
+            }
+
+          }).addTo(map);
+
+          control.on('routesfound', function (e) {
+            const routes = e.routes;
+            const summary = routes[0].summary;
+            setDistance(`${(summary.totalDistance / 1000).toFixed(1)} km`);
+            setInstructions(routes[0].instructions.map((instr, index) => (
+              <div key={index} className="instruction-item">
+              <span>{instr.text}</span>
+              <span>{instr.distance.toFixed(0)} m</span>
+            </div>
+            )));
+          });
+
+          setRoutingControl(control);
+        }
+      }
+    }, [origin, destination, map, routingControl]);
+    return null;
+  };
+
   const handleMapLoad = () => {
     setMapReady(true);
+  };
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
   };
 
   return (
@@ -155,16 +213,15 @@ export const CreateTravelComponent = () => {
       <Button onClick={() => navigate(-1)}>Volver</Button>
       <h2>Crear Viaje</h2>
       <Form
-        labelCol={{ span: 5 }}
-        wrapperCol={{ span: 19 }}
-        labelWrap={true}
+        layout="inline"
+        className="inline-form"
         onFinish={handleSubmit}>
-        <Form.Item label="Distancia" name="distance" rules={[{ required: true }]}>
+        <Form.Item label="Distancia">
           <Input
             id="distance"
             value={distance}
-            onChange={(e) => setDistance(e.target.value)}
-            required />
+            readOnly
+          />
         </Form.Item>
 
         <Form.Item label="Origen" name="origin" rules={[{ required: true }]}>
@@ -208,10 +265,12 @@ export const CreateTravelComponent = () => {
         <Button type="primary" htmlType="submit">Crear Viaje</Button>
       </Form>
 
-      <MapContainer center={mapCenter} zoom={6} style={{ height: "400px", width: "100%" }} whenCreated={handleMapLoad}>
+      {/* MAPA */}
+      <MapContainer center={mapCenter} zoom={6} style={{ height: "400px", width: "100%"}} whenCreated={handleMapLoad}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        
         <MapUpdater bounds={bounds} />
         {origin && (
           <Marker position={origin} icon={originIcon}>
@@ -227,7 +286,16 @@ export const CreateTravelComponent = () => {
             </Popup>
           </Marker>
         )}
+        <RouteDrawer origin={origin} destination={destination} />
       </MapContainer>
+
+      {/* INSTRUCCIONES */}
+      <Button type="primary" onClick={showModal}>
+        Ver Instrucciones
+      </Button>
+      <Modal title="Instrucciones de Ruta" open={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+        <div className="modal-content">{instructions}</div>
+      </Modal>
     </>
   );
 };
