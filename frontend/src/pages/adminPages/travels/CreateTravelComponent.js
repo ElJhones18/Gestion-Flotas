6c2ba09b5df45b3fda0a0e11bf311ec6fb95974b
JulianaChM <julianchamorromarin@gmail.com>
2024-06-17 06:37:07 -0500

    feat:draw stops on the map

--- frontend/src/pages/adminPages/travels/CreateTravelComponent.js
+++ frontend/src/pages/adminPages/travels/CreateTravelComponent.js
@@ -1,24 +1,34 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Button, Input, Select, Form, Modal } from "antd";
+import { Button, Input, Select, Form, Modal, Divider } from "antd";
 import { PATHS } from "../../../utils/config";
 import axios from "axios";
 import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import {
+  CloseCircleFilled,
+  CloseCircleOutlined,
+  CloseOutlined,
+  LeftCircleFilled,
+  LeftCircleOutlined,
+  PlusCircleFilled,
+  PlusCircleOutlined,
+} from '@ant-design/icons';
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import "leaflet-routing-machine";
 import iconGreen from "../../../uploads/marks/green-mark.svg";
 import iconRed from "../../../uploads/marks/red-mark.svg";
 import AsyncSelect from 'react-select/async';
+import { MarkerComponent } from './StopsMark'; // Import the MarkerComponent
 import './CreateTravelComponent.css';
 
 const { Option } = Select;
 
+const MARKER_PATH = 'M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0z';
+
 export const CreateTravelComponent = () => {
   const navigate = useNavigate();
   const [distance, setDistance] = useState('');
-  const [origin, setOrigin] = useState(null);
-  const [destination, setDestination] = useState(null);
   const [mapCenter, setMapCenter] = useState([4.570868, -74.297333]); // Center map on Colombia
   const [mapReady, setMapReady] = useState(false);
   const [isModalVisible, setIsModalVisible] = useState(false);
@@ -31,20 +41,31 @@ export const CreateTravelComponent = () => {
   const [drivers, setDrivers] = useState([]);
   const [selectedDriver, setSelectedDriver] = useState('');
 
+  const [waypoints, setWaypoints] = useState([{ id: 1, location: null }]);
+
   const originIcon = L.icon({
-    iconUrl: iconGreen, 
-    iconSize: [25, 41], 
-    iconAnchor: [12, 41], 
+    iconUrl: iconGreen,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
     popupAnchor: [1, -34],
     shadowSize: [41, 41]
   });
 
   const destinationIcon = L.icon({
-    iconUrl: iconRed, 
-    iconSize: [25, 41], 
+    iconUrl: iconRed,
+    iconSize: [25, 41],
     iconAnchor: [12, 41],
     popupAnchor: [1, -34],
     shadowSize: [41, 41]
+  });
+
+  const intermediateIcon = (number) => L.divIcon({
+    /* <MarkerComponent color="blue" number={number} size={50} cursor="ns-resize" /> */
+    html: `<div style="position: relative;"><svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" height="37" stroke: none;><path d="${MARKER_PATH}" fill="#76d0f7" /><circle cx="190" cy="190" r="110" fill="white" /><text x="50%" y="50%" text-anchor="middle" style="font-size: 180px;" fill="black">${number}</text></svg></div>`,
+    className: "custom-icon",
+    iconSize: [50, 50],
+    iconAnchor: [25, 50],
+    popupAnchor: [0, -50]
   });
 
   const fetchSuggestions = async (inputValue) => {
@@ -52,7 +73,7 @@ export const CreateTravelComponent = () => {
       return [];
     }
     try {
-      const response = await axios.get(`https://graphhopper.com/api/1/geocode?q=${inputValue}, Colombia&locale=es&key=bc2c1444-a0c2-4cad-8814-6f196228c9fe`);
+      const response = await axios.get(`https://graphhopper.com/api/1/geocode?q=${inputValue}&locale=es&key=bc2c1444-a0c2-4cad-8814-6f196228c9fe`);
       return response.data.hits.map(hit => ({
         label: `${hit.name}, ${hit.country}`,
         value: [hit.point.lat, hit.point.lng],
@@ -63,23 +84,29 @@ export const CreateTravelComponent = () => {
     }
   };
 
-  const handleOriginChange = (selectedOption) => {
-    setOrigin(selectedOption.value);
-    updateBounds(selectedOption.value, destination);
+  const handleWaypointChange = (index, selectedOption) => {
+    const newWaypoints = [...waypoints];
+    newWaypoints[index].location = selectedOption.value;
+    setWaypoints(newWaypoints);
+    updateBounds(newWaypoints);
   };
 
-  const handleDestinationChange = (selectedOption) => {
-    setDestination(selectedOption.value);
-    updateBounds(origin, selectedOption.value);
+  const addWaypoint = () => {
+    setWaypoints([...waypoints, { id: waypoints.length + 1, location: null }]);
   };
 
-  const updateBounds = (origin, destination) => {
-    if (origin && destination) {
-      setBounds([origin, destination]);
-    } else if (origin) {
-      setBounds([origin]);
-    } else if (destination) {
-      setBounds([destination]);
+  const removeWaypoint = (index) => {
+    const newWaypoints = waypoints.filter((_, wpIndex) => wpIndex !== index);
+    setWaypoints(newWaypoints);
+    updateBounds(newWaypoints);
+  };
+
+  const updateBounds = (waypoints) => {
+    const bounds = waypoints
+      .map(waypoint => waypoint.location)
+      .filter(location => location !== null);
+    if (bounds.length > 0) {
+      setBounds(bounds);
     }
   };
 
@@ -88,8 +115,7 @@ export const CreateTravelComponent = () => {
   const handleSubmit = async () => {
     const travel = {
       distance,
-      origin,
-      destination,
+      waypoints: waypoints.map(wp => wp.location),
       driverId: selectedDriver,
       truckId: selectedTruck
     };
@@ -98,7 +124,7 @@ export const CreateTravelComponent = () => {
       const URL = PATHS.BASE_PATH + PATHS.API_ROUTES.CREATE_TRAVEL;
       const response = await axios.post(URL, travel, {
         headers: {
-            'Content-Type': 'application/json'
+          'Content-Type': 'application/json'
         }
       });
       console.log(response);
@@ -112,7 +138,7 @@ export const CreateTravelComponent = () => {
     fetchTrucks();
   }, []);
 
-  const fetchDrivers = async () =>  {
+  const fetchDrivers = async () => {
     try {
       const URL = PATHS.BASE_PATH + PATHS.API_ROUTES.LIST_USERS;
       const response = await axios.get(URL);
@@ -143,34 +169,28 @@ export const CreateTravelComponent = () => {
     return null;
   };
 
-  const RouteDrawer = ({ origin, destination }) => {
+  const RouteDrawer = ({ waypoints }) => {
     const map = useMap();
-    const instructionsRef = useRef();
 
     useEffect(() => {
-      if (origin && destination) {
+      if (waypoints.length > 1 && waypoints.every(wp => wp.location !== null)) {
+        const locations = waypoints.map(wp => L.latLng(wp.location[0], wp.location[1]));
+
         if (routingControl) {
-          routingControl.getPlan().setWaypoints([
-            L.latLng(origin[0], origin[1]),
-            L.latLng(destination[0], destination[1])
-          ]);
+          routingControl.getPlan().setWaypoints(locations);
         } else {
           const control = L.Routing.control({
-            waypoints: [
-              L.latLng(origin[0], origin[1]),
-              L.latLng(destination[0], destination[1])
-            ],
+            waypoints: locations,
             language: 'es',
-            createMarker: function(i, wp) {
+            createMarker: function (i, wp) {
               return L.marker(wp.latLng, {
-                icon: i === 0 ? originIcon : destinationIcon
+                icon: i === 0 ? originIcon : i === waypoints.length - 1 ? destinationIcon : intermediateIcon(i + 1)
               });
             },
             routeWhileDragging: true,
             lineOptions: {
               styles: [{ color: '#5980ad', opacity: 1, weight: 5 }]
             }
-
           }).addTo(map);
 
           control.on('routesfound', function (e) {
@@ -179,16 +199,16 @@ export const CreateTravelComponent = () => {
             setDistance(`${(summary.totalDistance / 1000).toFixed(1)} km`);
             setInstructions(routes[0].instructions.map((instr, index) => (
               <div key={index} className="instruction-item">
-              <span>{instr.text}</span>
-              <span>{instr.distance.toFixed(0)} m</span>
-            </div>
+                <span>{instr.text}</span>
+                <span>{instr.distance.toFixed(0)} m</span>
+              </div>
             )));
           });
 
           setRoutingControl(control);
         }
       }
-    }, [origin, destination, map, routingControl]);
+    }, [waypoints, map, routingControl]);
     return null;
   };
 
@@ -208,91 +228,134 @@ export const CreateTravelComponent = () => {
     setIsModalVisible(false);
   };
 
+  const cleanField = (index) => {
+    setWaypoints(null);
+  };
+
   return (
     <>
-      <Button onClick={() => navigate(-1)}>Volver</Button>
-      <h2>Crear Viaje</h2>
-      <Form
-        layout="inline"
-        className="inline-form"
-        onFinish={handleSubmit}>
-        <Form.Item label="Distancia">
-          <Input
-            id="distance"
-            value={distance}
-            readOnly
-          />
-        </Form.Item>
+      <div style={{
+        display: 'flex',
+        alignItems: 'center',
+      }}>
+        <Button type="danger" onClick={() => navigate(-1)}><LeftCircleOutlined style={{
+          fontSize: '25px',
+        }} /></Button>
+        <h2>Crear Viaje</h2>
+      </div>
+      <div className="form-container">
+        <div className="form-column column-left">
+        <Form layout="vertical" onFinish={handleSubmit} style={{ margin: "20px" }}>
 
-        <Form.Item label="Origen" name="origin" rules={[{ required: true }]}>
-          <AsyncSelect
-            cacheOptions
-            loadOptions={fetchSuggestions}
-            onChange={handleOriginChange}
-            defaultOptions
-          />
-        </Form.Item>
 
-        <Form.Item label="Destino" name="destination" rules={[{ required: true }]}>
-          <AsyncSelect
-            cacheOptions
-            loadOptions={fetchSuggestions}
-            onChange={handleDestinationChange}
-            defaultOptions
-          />
-        </Form.Item>
 
-        <Form.Item label="Conductor" name="driver" rules={[{ required: true }]}>
-          <Select value={selectedDriver} onChange={(value) => setSelectedDriver(value)}>
-            {drivers.map((driver) => (
-              <Option key={driver.id} value={driver.id}>
-                {driver.username} {driver.lastname}
-              </Option>
-            ))}
-          </Select>
-        </Form.Item>
+          <Form.Item label="Origen" rules={[{ required: true }]}>
+            <AsyncSelect
+              components={{ DropdownIndicator: () => null, IndicatorSeparator: () => null }}
+              placeholder="Desde"
+              cacheOptions
+              loadOptions={fetchSuggestions}
+              onChange={selectedOption => handleWaypointChange(0, selectedOption)}
+              defaultOptions
+            />
+          </Form.Item>
+          {waypoints.slice(1, waypoints.length - 1).map((waypoint, index) => (
+            <Form.Item key={waypoint.id} label={`Parada ${index + 1}`} style={{ display: 'flex', alignItems: 'center' }}>
+              <div style={{ flex: 1, display: 'flex', alignItems: 'center' }}>
+                <AsyncSelect
+                  components={{ DropdownIndicator: () => null, IndicatorSeparator: () => null }}
+                  placeholder="Pasando por"
+                  cacheOptions
+                  loadOptions={fetchSuggestions}
+                  onChange={selectedOption => handleWaypointChange(index + 1, selectedOption)}
+                  defaultOptions
+                  styles={{
+                    container: base => ({
+                      ...base,
+                      flexGrow: 1,
+                    }),
+                    control: base => ({
+                      ...base,
+                      height: '38px',
+                      minWidth: '250px',
+                    }),
+                  }}
+                />
+                <Button type="danger" onClick={() => removeWaypoint(index + 1)} style={{ marginLeft: "5px", padding: "0px", display: 'flex', alignItems: 'center', height: '38px' }}>
+                  <CloseCircleOutlined style={{ fontSize: '20px' }} /></Button>
+              </div>
+            </Form.Item>
+          ))}
+          <Form.Item label="Destino" rules={[{ required: true }]}>
+            <AsyncSelect
+              components={{ DropdownIndicator: () => null, IndicatorSeparator: () => null }}
+              placeholder="Hasta"
+              cacheOptions
+              loadOptions={fetchSuggestions}
+              onChange={selectedOption => handleWaypointChange(waypoints.length - 1, selectedOption)}
+              defaultOptions
+            />
+          </Form.Item>
+          <Button type="danger" onClick={addWaypoint} style={{alignContent:"center", marginBottom:"6px", padding:"0"}}>
+            <PlusCircleOutlined style={{
+              fontSize: '20px', height: '10px',
+            }} />Añadir ubicación
+          </Button>
+          <Form.Item label="Distancia">
+            <Input id="distance" value={distance} readOnly />
+          </Form.Item>
+          <Divider style={{marginBottom: "14px"}}/>
+          <Form.Item label="Conductor" name="driver" rules={[{ required: true }]}>
+            <Select value={selectedDriver} onChange={(value) => setSelectedDriver(value)}>
+              {drivers.map((driver) => (
+                <Option key={driver.id} value={driver.id}>
+                  {driver.username} {driver.lastname}
+                </Option>
+              ))}
+            </Select>
+          </Form.Item>
+          <Form.Item label="Camión" name="truckId" rules={[{ required: true }]}>
+            <Select value={selectedTruck} onChange={(value) => setSelectedTruck(value)}>
+              {trucks.map((truck) => (
+                <Option key={truck.id} value={truck.id}>
+                  {truck.plate} - {truck.model}
+                </Option>
+              ))}
+            </Select>
+          </Form.Item>
 
-        <Form.Item label="Camión" name="truckId" rules={[{ required: true }]}>
-          <Select value={selectedTruck} onChange={(value) => setSelectedTruck(value)}>
-            {trucks.map((truck) => (
-              <Option key={truck.id} value={truck.id}>
-                {truck.plate} - {truck.model}
-              </Option>
-            ))}
-          </Select>
-        </Form.Item>
 
-        <Button type="primary" htmlType="submit">Crear Viaje</Button>
-      </Form>
-
+          
+          <div style={{ display: "flex", justifyContent: "flex-end", marginTop:"10px" }}>
+            <Button type="primary" htmlType="submit">Crear Viaje</Button>
+          </div>
+        </Form>
+         </div>
+         <div className="form-column">
       {/* MAPA */}
-      <MapContainer center={mapCenter} zoom={6} style={{ height: "400px", width: "100%"}} whenCreated={handleMapLoad}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        
+      <MapContainer center={mapCenter} zoom={6} style={{ height: "430px", width: "100%" }} whenCreated={handleMapLoad}>
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
         <MapUpdater bounds={bounds} />
-        {origin && (
-          <Marker position={origin} icon={originIcon}>
-            <Popup>
-              Origen
-            </Popup>
-          </Marker>
-        )}
-        {destination && (
-          <Marker position={destination} icon={destinationIcon}>
-            <Popup>
-              Destino
-            </Popup>
-          </Marker>
-        )}
-        <RouteDrawer origin={origin} destination={destination} />
+        {waypoints.map((waypoint, index) => (
+          waypoint.location && (
+            <Marker key={index} position={waypoint.location} icon={index === 0 ? originIcon : index === waypoints.length - 1 ? destinationIcon : intermediateIcon(index + 1)}>
+              <Popup>
+                {index === 0 ? "Origen" : index === waypoints.length - 1 ? "Destino" : `Parada ${index}`}
+              </Popup>
+            </Marker>
+          )
+        ))}
+        <RouteDrawer waypoints={waypoints} />
       </MapContainer>
 
       {/* INSTRUCCIONES */}
+      <div style={{ display: "flex", justifyContent: "center", marginTop: "10px" }}>
       <Button type="primary" onClick={showModal}>
         Ver Instrucciones
       </Button>
+      </div>
+      </div>
+      </div>
       <Modal title="Instrucciones de Ruta" open={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
         <div className="modal-content">{instructions}</div>
       </Modal>
---
+++ frontend/src/pages/adminPages/travels/StopsMark.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const MARKER_PATH =
+    'M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0z';
+const INNER_CIRCLE = 'M192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z';
+
+const markerStyle = {
+    cursor: 'pointer',
+    stroke: 'none',
+};
+
+/**
+ * This component draws a marker. If a number is passed, the white circle of the marker is larger and displays the
+ * number. Otherwise the default marker from https://fontawesome.com/v5.15/icons/map-marker-alt?style=solid is taken
+ */
+export function MarkerComponent({ color, number, size = 0, cursor }) {
+    return (
+        <svg
+            aria-hidden="true"
+            focusable="false"
+            role="img"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 384 512"
+            height={size}
+            style={{ ...markerStyle, fill: color, cursor: cursor ? cursor : markerStyle.cursor }}
+        >
+            <path d={MARKER_PATH} />
+            {number === undefined ? (
+                <path d={INNER_CIRCLE} fill="white" />
+            ) : (
+                <circle cx="192" cy="190" r="150" fill="white" />
+            )}
+            <text x="50%" y="55%" textAnchor="middle" style={{ fontSize: 210 }} fill="black">
+                {number !== undefined ? number : ''}
+            </text>
+        </svg>
+    );
+}
